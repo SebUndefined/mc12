3af6fc4b4d97ec8e919ce57dd5a8bd0d0a2db8c7
SebUndefined <me@sebundefined.fr>
2017-09-21 15:47:03 +0200

    AdminBundle created. Needs a UserBundle ?

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new MC12\SubscriptionBundle\MC12SubscriptionBundle(),
+            new MC12\AdminBundle\MC12AdminBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/MC12/AdminBundle/MC12AdminBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MC12\AdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MC12AdminBundle extends Bundle
+{
+}
